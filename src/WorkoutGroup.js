cf352c35b0cd0ffebc05258c0893d06bd8cb72f8
Greg Leppert <falquan@falquan.com>
2018-08-30 00:06:14 -0700

    fix merge

--- src/WorkoutGroup.js
+++ src/WorkoutGroup.js
@@ -9,21 +9,12 @@ class WorkoutGroup extends Component {
     console.log(this.props.hierarchicalWorkout);
 
     return (
-<<<<<<< HEAD
       <div className="WorkoutGroup-workoutGroup">
         <div className="WorkoutGroup-workouts">
           <Workout
             hierarchicalWorkout={this.props.hierarchicalWorkout} />
         </div>
         <div className="WorkoutGroup-displayName">{this.props.selectedBlock}</div>
-=======
-      <div className="WorkoutGroup-workouts">
-        <h3>{this.props.selectedSheet}</h3>
-        {/* <Workout
-          workouts={this.props.workoutGroups.values}
-          selectedSheet={this.props.selectedSheet}
-          hierarchicalWorkout={this.props.hierarchicalWorkout} /> */}
->>>>>>> 0c393b28deb34cb694eb8539de8d1acccdb83189
       </div>
       );
   }
